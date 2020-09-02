4fa8081c70112f988fa42bc22959f83773311f0e
Samuel Gratzl <sam@sgratzl.com>
2020-09-02 14:29:39 +0200

    build only limited when watching

--- rollup.config.js
+++ rollup.config.js
@@ -18,62 +18,70 @@ const banner = `/**
  */
 `;
 
-export default [
-  {
-    input: './src/index.ts',
-    output: [
-      {
-        sourcemap: true,
-        file: pkg.module,
-        format: 'esm',
-        banner,
-      },
-      {
-        sourcemap: true,
-        file: pkg.main,
-        format: 'cjs',
-        banner,
-      },
-      {
-        sourcemap: true,
-        file: pkg.unpkg,
-        format: 'umd',
-        name: pkg.global,
-        banner,
-        plugins: [terser()],
-      },
-    ],
-    external: Object.keys(pkg.dependencies || {}).concat(Object.keys(pkg.peerDependencies || {})),
-    plugins: [
-      typescript(),
-      resolve(),
-      commonjs(),
-      replace({
-        // eslint-disable-next-line no-undef
-        'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV) || 'production',
-        __VERSION__: JSON.stringify(pkg.version),
-      }),
-      cleanup({
-        comments: ['some', 'ts', 'ts3s'],
-        extensions: ['ts', 'tsx', 'js', 'jsx'],
-        include: './src/**/*',
-      }),
-    ],
-  },
-  {
-    input: './src/index.ts',
-    output: [
-      {
-        file: pkg.types,
-        format: 'es',
-        banner,
-      },
-    ],
-    external: Object.keys(pkg.dependencies || {}).concat(Object.keys(pkg.peerDependencies || {})),
-    plugins: [
-      dts({
-        respectExternal: true,
-      }),
-    ],
-  },
-];
+/**
+ * defines which formats (umd, esm, cjs, types) should be built when watching
+ */
+const watchOnly = ['umd'];
+
+export default (options) => {
+  const commonOutput = {
+    sourcemap: true,
+    banner,
+  };
+  return [
+    {
+      input: './src/index.ts',
+      output: [
+        (!options.watch || watchOnly.includes('esm')) && {
+          ...commonOutput,
+          file: pkg.module,
+          format: 'esm',
+        },
+        (!options.watch || watchOnly.includes('cjs')) && {
+          ...commonOutput,
+          file: pkg.main,
+          format: 'cjs',
+        },
+        (!options.watch || watchOnly.includes('umd')) && {
+          ...commonOutput,
+          file: pkg.unpkg,
+          format: 'umd',
+          name: pkg.global,
+          plugins: [terser()],
+        },
+      ].filter(Boolean),
+      external: Object.keys(pkg.dependencies || {}).concat(Object.keys(pkg.peerDependencies || {})),
+      plugins: [
+        typescript(),
+        resolve(),
+        commonjs(),
+        replace({
+          // eslint-disable-next-line no-undef
+          'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV) || 'production',
+          __VERSION__: JSON.stringify(pkg.version),
+        }),
+        cleanup({
+          comments: ['some', 'ts', 'ts3s'],
+          extensions: ['ts', 'tsx', 'js', 'jsx'],
+          include: './src/**/*',
+        }),
+      ],
+    },
+    (!options.watch || watchOnly.includes('types')) && {
+      input: './src/index.ts',
+      output: [
+        {
+          file: pkg.types,
+          format: 'es',
+          banner,
+        },
+      ],
+      external: Object.keys(pkg.dependencies || {}).concat(Object.keys(pkg.peerDependencies || {})),
+      plugins: [
+        dts({
+          respectExternal: true,
+        }),
+      ],
+    },
+  ].filter(Boolean);
+};
--- src/index.spec.ts
+++ src/index.spec.ts
@@ -2,6 +2,6 @@ import { helloWorld } from '.';
 
 describe('test', () => {
   test('a', () => {
-    expect(helloWorld()).toBe('hello-world');
+    expect(helloWorld()).toBe('hello-world0,1,2,3,4,5,6,7,8,9');
   });
 });
